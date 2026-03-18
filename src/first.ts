97871817940fa94fd43edb557ef1e5a933b4a97a
sandun18 <sandunanjana333@gmail.com>
2026-03-19 02:07:16 +0530

    Add initial TypeScript files with basic variable declarations and console logs

---
+++ src/first.ts
@@ -0,0 +1,5 @@
+let isActive: boolean = true;
+let hasPermission: boolean = false;
+
+console.log(isActive);
+console.log(hasPermission);
---
+++ src/test1.ts
@@ -0,0 +1,14 @@
+let color: string= "red";
+let fullName: string = 'John Doe';
+let age: number = 30;
+
+let sentence: string =`Hello, my name is ${fullName}. I am ${age} years old and my favorite color is ${color}.`;
+
+
+console.log(color);
+console.log(fullName);
+console.log(age);
+console.log(sentence);
+
+
+
